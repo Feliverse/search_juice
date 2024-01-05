6c59e169b708c8e1b94cc01f610e4edb6c74fa90
Feliverse <114632980+Feliverse@users.noreply.github.com>
2024-01-05 04:24:21 -0400

    Update to Render

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,4 @@
 class SearchesController < ApplicationController
-  def search
-    @results = 
-    Article.where("column_name LIKE ?", "%#{params[:query]}%")
-  end
   def process_search
     @query = params[:query]
     record_search(@query)
@@ -19,4 +15,14 @@ class SearchesController < ApplicationController
     # Lógica para almacenar consultas relevantes
     SearchQuery.create(query: query, ip_address: request.remote_ip)
   end
+  
+  def statistics
+    @most_common_searches = SearchQuery.group(:query).count
+  
+    @top_searches = @most_common_searches.sort_by { |_query, count| -count }.take(5)
+  
+    puts "Top searches: #{@top_searches}" # Esto imprimirá en la consola de Rails
+    logger.debug "Top searches: #{@top_searches}" # Esto también lo puedes ver en los logs
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "searches#search"
 
-  get '/searches', to: 'searches#search'  # Ruta para mostrar el formulario de búsqueda
-  get '/process_search', to: 'searches#process_search'  # Ruta para procesar la búsqueda
+  get '/searches', to: 'searches#search'  
+  get '/process_search', to: 'searches#process_search'  
+  get '/statistics', to: 'searches#statistics', as: 'statistics'
 end
