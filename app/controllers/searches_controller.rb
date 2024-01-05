d17bf6d7531f824351fc982b73828d98870be9de
Feliverse <114632980+Feliverse@users.noreply.github.com>
2024-01-05 02:50:03 -0400

    Update models to save IP

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,18 @@ class SearchesController < ApplicationController
   end
   def process_search
     @query = params[:query]
+    record_search(@query)
+
     @articles = Article.where("title LIKE ? OR body LIKE ?", "%#{@query}%", "%#{@query}%")
+    render 'searches/process_search', articles: @articles, query: @query
+  end
+
+  private
+
+  def record_search(query)
+    return if query.blank? || query.split.size <= 3 # Ignorar consultas cortas o incompletas
+
+    # Lógica para almacenar consultas relevantes
+    SearchQuery.create(query: query, ip_address: request.remote_ip)
   end
 end
