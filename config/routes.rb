3a1b088ffb4725543bb51654bfd10ce348e18cf3
Feliverse <114632980+Feliverse@users.noreply.github.com>
2024-01-04 23:39:11 -0400

    Generate Articles model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/searches', to: 'searches#search'  # Ruta para mostrar el formulario de búsqueda
+  get '/process_search', to: 'searches#process_search'  # Ruta para procesar la búsqueda
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Crear artículos de ejemplo
+Article.create(title: "Primer artículo", body: "Contenido del primer artículo")
+Article.create(title: "Segundo artículo", body: "Contenido del segundo artículo")
+# Puedes agregar más artículos aquí según sea necesario
